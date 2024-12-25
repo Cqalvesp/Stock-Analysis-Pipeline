f1e22b2dcc0ab0929911c1b9cf9d47001a0fde7f
Christian Alves-Patterson <98926428+Cqalvesp@users.noreply.github.com>
2024-12-25 03:41:18 -0500

    Data collection files

---
+++ collector.py
@@ -0,0 +1,16 @@
+# File to pull data from Yahoo Finance API
+# Retrieves data based on given financial metrics
+# Pulled data: Current stock price, number of splits, dividend rates, company valuation, etc.
+import yfinance as yf
+
+# Create object called stock
+# Stock will have a ticker
+# Traits will be historical data, financials, and actions
+
+# Any or all of the traits can be displayed
+
+# Initialized with a ticker symbol
+
+    
+    
+    
